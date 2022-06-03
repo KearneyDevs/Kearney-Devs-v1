29c3f92014d3a3ec63fd6c4f327f5c9fe606e075
= <shanekearney91@gmail.com>
2022-06-03 15:55:53 +0100

    Adding tutorials on cypress using http methods

--- cypress/integration/GETProdutos.spec.js
+++ cypress/integration/GETProdutos.spec.js
@@ -1,10 +1,11 @@
 describe('Getting Products from the API', ()=> {
     context('GET method to /produtos', () => {
         it('should return a list with all products', () => {
+            // Perform a request that gets items from the api
             cy.request({
                 method: 'GET',
                 url: 'https://serverest.dev/produtos'
-            })
+            }) // Response object below of what to expect
             .should((response) => {
                 expect(response.status).to.eq(200)
                 expect(response.body.quantidade).to.eq(3)
---
+++ cypress/integration/Usuarios/GETUsuarios.spec.js
@@ -0,0 +1,41 @@
+describe('Given the Users API', () => {
+    context('When I send GET /usuarios', ()=>{
+        it('Then it should return a list with all registered users', ()=>{
+            // place your tests here
+            cy.request({
+                method: 'GET',
+                url: '/usuarios'
+            })
+            .should((response) => {
+                // all assertions should be placed here
+                expect(response.status).to.eq(200)
+                expect(response.body.quantidade).to.eq(response.body.usuarios.length)
+                // Tests each item within the usuarios and that they should not equal null
+                Cypress._.each(response.body.usuarios, (usuario) => {
+                    expect(usuario.email).to.not.be.null
+                    // expect all items to have the following keys within the object
+                    expect(usuario).to.have.all.keys('nome', 'email', 'password', 'administrador', '_id')
+                })
+                
+                // cy.log(JSON.stringify(response.body))
+            })
+        });
+    });
+
+    context('When I send GET /usuarios passing id query param', () =>{
+        it('Then it should return only the filtered user', ()=>{
+            // place your tests here
+            cy.request({
+                method: 'GET',
+                url: '/usuarios',
+                qs: {
+                    _id: '0uxuPY0cbmQhpEz1'
+                }
+            })
+            .should((response) => {
+                expect(response.status).to.eq(200)
+                expect(response.body.usuarios[0].nome).to.eq('Fulano da Silva')
+            });
+        });
+    });
+})
