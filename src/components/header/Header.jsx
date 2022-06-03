80640d6c88c657dd51ba73defb92f8df2add7747
= <shanekearney91@gmail.com>
2022-06-03 12:59:12 +0100

    Removed styling from Header component and created a Header styled component and imported

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,25 +1,25 @@
-import styled from 'styled-components';
+import { Section } from '../styled/Styles';
+import { Nav } from './Header.style';
 import { AiOutlineMenu } from 'react-icons/ai';
 import logo from '../../imgs/Icon2.svg'
-
-const Nav = styled.nav`
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-`
 
 const Header = props => {
 
     console.log(props);
     
     return (
-        <Nav>
-            <a href="">
-                <img src={logo} width="57px" height="18px" alt="Shane Kearney" />
-            </a>
+        <Section>
+            <Nav>
+                <a href="#">
+                    <img src={logo} width="57px" height="18px" alt="Shane Kearney" />
+                </a>
 
-            <AiOutlineMenu size={32} color="#1BDFB0" />
-        </Nav>
+                <a href="#">
+                    <AiOutlineMenu size={32} color="#1BDFB0" />
+                </a>
+                
+            </Nav>
+        </Section>
     )
 }
 
