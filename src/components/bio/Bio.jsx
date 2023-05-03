fade175ddbc04102e083f63ad03b1b879ebb610f
= <shanekearney91@gmail.com>
2023-05-03 20:41:25 +0100

    removed console log

--- src/components/bio/Bio.jsx
+++ src/components/bio/Bio.jsx
@@ -1,29 +1,25 @@
-import {Title, Section} from '../styled/Styles';
-import { Table } from './Bio.style';
+import { Title, Section } from "../styled/Styles";
+import { Table } from "./Bio.style";
 
-const Bio = props => {
+const Bio = (props) => {
+  return (
+    <Section>
+      <Title>{props.section.title}</Title>
 
-    console.log(props);
-
-    return (
-        <Section>
-            <Title>{props.section.title}</Title>
-            
-            <Table>
-                <tbody>
-                    {props.section.content.map(tr => {
-                        return (
-                            <tr key={tr.id}>
-                                <td>{tr.date}</td>
-                                <td>{tr.context}</td>
-                            </tr>
-                        )
-                    })}
-                    
-                </tbody>
-            </Table>            
-        </Section>
-    )
-}
+      <Table>
+        <tbody>
+          {props.section.content.map((tr) => {
+            return (
+              <tr key={tr.id}>
+                <td>{tr.date}</td>
+                <td>{tr.context}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </Table>
+    </Section>
+  );
+};
 
 export default Bio;
