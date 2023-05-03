3583ecbd170ac5ae773b8c2383dd856282ef566e
= <shanekearney91@gmail.com>
2023-05-03 22:09:51 +0100

    Updated anchor tags with attributes rel

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -34,6 +34,7 @@ const Profile = (props) => {
           <a
             href="https://www.google.com/maps/place/Armagh/@54.34702,-6.6485549,13z/data=!3m1!4b1!4m6!3m5!1s0x48608d557dcb168d:0xb92e637dcdc91848!8m2!3d54.3502798!4d-6.652792!16zL20vMDJkdHFz"
             target="_blank"
+            rel="noreferrer"
           >
             Ard Mhacha
           </a>
@@ -41,6 +42,7 @@ const Profile = (props) => {
           <a
             href="https://www.google.com/maps/place/Ireland/@53.3053169,-13.2322336,6z/data=!3m1!4b1!4m6!3m5!1s0x485e619e5d73698f:0xca9b39444d6ac68d!8m2!3d53.7797554!4d-7.3055309!16zL20vMDEyd2di"
             target="_blank"
+            rel="noreferrer"
           >
             Éire
           </a>
@@ -48,6 +50,7 @@ const Profile = (props) => {
           <a
             href="https://www.google.com/maps/place/Edinburgh/@55.941153,-3.5349628,10z/data=!3m1!4b1!4m6!3m5!1s0x4887b800a5982623:0x64f2147b7ce71727!8m2!3d55.953252!4d-3.188267!16zL20vMDJtNzc"
             target="_blank"
+            rel="noreferrer"
           >
             Edinburgh
           </a>
