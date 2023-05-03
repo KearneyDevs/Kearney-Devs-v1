2dfeb4c08cb45611aa3981b3e66dba20306a9f08
= <shanekearney91@gmail.com>
2023-05-03 20:40:51 +0100

    Updated component to map paragraph from data.js

--- src/components/interests/Interests.jsx
+++ src/components/interests/Interests.jsx
@@ -1,11 +1,15 @@
-import {Title, Section} from '../styled/Styles';
+import { Title, Section } from "../styled/Styles";
 
-const Interests = props => {
-    return (
-        <Section>
-            <Title>{props.section.title}</Title>
-        </Section>
-    )
-}
+const Interests = (props) => {
+  return (
+    <Section>
+      <Title>{props.section.title}</Title>
+
+      {props.section.content.map((p) => {
+        return <p key={p.id}>{p.paragraph}</p>;
+      })}
+    </Section>
+  );
+};
 
 export default Interests;
