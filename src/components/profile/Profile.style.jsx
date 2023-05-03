1a309f7f562be0246a104086ac9fe608affe02c0
= <shanekearney91@gmail.com>
2023-05-03 22:07:48 +0100

    Created a style for content

--- src/components/profile/Profile.style.jsx
+++ src/components/profile/Profile.style.jsx
@@ -1,55 +1,64 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const ProfileContainer = styled.div`
-display: flex;
-flex-direction: column;
-align-items: center;
-margin: 4rem 0 2rem 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 4rem 0 2rem 0;
 
-img {
+  img {
     width: 150px;
     height: 150px;
     border-radius: 50%;
-    border: ${props => `3px solid ${props.theme.colors.primary}`};
+    border: ${(props) => `3px solid ${props.theme.colors.primary}`};
 
     background-repeat: no-repeat;
     background-position: center center;
     background-size: cover;
-    filter: drop-shadow(0px 8px 5px rgba(0,0,0,0.25));
-}
+    filter: drop-shadow(0px 8px 5px rgba(0, 0, 0, 0.25));
+  }
 
-h1 {
+  h1 {
     margin: 1rem 0 0 0;
-}
+  }
 
-p {
+  p {
     margin: 0;
-    color: ${props => props.theme.colors.secondary};
-}
-`
+    color: ${(props) => props.theme.colors.secondary};
+  }
+`;
 
 export const Table = styled.table`
+  width: 100%;
+  max-width: 450px;
+  margin-bottom: 2rem;
 
-    width: 100%;
-    max-width: 450px;
-    margin-bottom: 2rem;
-    
-    tr {
-        display: flex;
-        justify-content: space-between;
-    }
+  tr {
+    display: flex;
+    justify-content: space-between;
+  }
 
-    td {
-        vertical-align: top;
-        color: ${props => props.theme.colors.secondary};
-        text-align: right;
-        line-height: 1.5rem;
-    }
+  td {
+    vertical-align: top;
+    color: ${(props) => props.theme.colors.secondary};
+    text-align: right;
+    line-height: 1.5rem;
+  }
 
-    td:first-child {
-        font-weight: 600;
-        padding-right: 2rem;  
-        color: ${props => props.theme.colors.primary};
-        text-align: left;
-    }
-`
+  td:first-child {
+    font-weight: 600;
+    padding-right: 2rem;
+    color: ${(props) => props.theme.colors.primary};
+    text-align: left;
+  }
+`;
+
+export const Content = styled.div`
+  a,
+  a:visited {
+    color: rgb(84, 255, 234);
+    opacity: 0.84;
+    font-weight: 600;
+    text-decoration: none;
+  }
+`;
