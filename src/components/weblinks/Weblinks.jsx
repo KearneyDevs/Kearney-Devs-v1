73d179a0032999c4d63fe24afd4a8dada8d0e30c
= <shanekearney91@gmail.com>
2023-05-03 20:39:25 +0100

    Updated to map from data rather than hardcoding each link with their respective icon

--- src/components/weblinks/Weblinks.jsx
+++ src/components/weblinks/Weblinks.jsx
@@ -1,66 +1,27 @@
-import { List } from './Weblinks.style';
-import {Title, Section} from '../styled/Styles';
-import { AiFillGithub } from 'react-icons/ai';
-import { AiOutlineTwitter } from 'react-icons/ai';
-import { AiOutlineInstagram } from 'react-icons/ai';
-import { AiFillLinkedin } from 'react-icons/ai';
+import { List } from "./Weblinks.style";
+import { Title, Section } from "../styled/Styles";
 
-const Weblinks = props => {
+const Weblinks = (props) => {
+  console.log(props);
 
-    console.log(props);
+  return (
+    <Section>
+      <Title>{props.section.title}</Title>
 
-    return (
-        <Section>
-            <Title>{props.section.title}</Title>
-            
-            <List>
-                <li key={props.section.content[0].id}>
-                    <AiFillGithub color="#1BDFB0" size={24} />
-                    <a 
-                        href={props.section.content[0].link}
-                        target="_blank"
-                        rel="noreferrer"
-                        >
-                        {props.section.content[0].title}
-                    </a>
-                </li>
-
-                <li key={props.section.content[1].id}>
-                    <AiOutlineTwitter color="#1BDFB0" size={24} />
-                    <a 
-                        href={props.section.content[1].link}
-                        target="_blank"
-                        rel="noreferrer"
-                        >
-                        {props.section.content[1].title}
-                    </a>
-                </li>
-
-                <li key={props.section.content[2].id}>
-                    <AiOutlineInstagram color="#1BDFB0" size={24} />
-                    <a 
-                        href={props.section.content[2].link}
-                        target="_blank"
-                        rel="noreferrer"
-                        >
-                        {props.section.content[2].title}
-                    </a>
-                </li>
-
-                <li key={props.section.content[3].id}>
-                    <AiFillLinkedin color="#1BDFB0" size={24} />
-                    <a 
-                        href={props.section.content[3].link}
-                        target="_blank"
-                        rel="noreferrer"
-                        >
-                        {props.section.content[3].title}
-                    </a>
-                </li>
-            </List>
-           
-        </Section>
-    )
-}
+      <List>
+        {props.section.content.map((webLink) => {
+          return (
+            <li key={webLink.id}>
+              <webLink.Icon color="#1BDFB0" size={24} />
+              <a href={webLink.link} target="_blank" rel="noreferrer">
+                {webLink.title}
+              </a>
+            </li>
+          );
+        })}
+      </List>
+    </Section>
+  );
+};
 
 export default Weblinks;
